418735b8aaa2c8c6ff12a5e8056f3931f1cca195
Michael Grill <Ultramar@Michaels-MacBook-Pro.local>
2019-03-23 12:23:37 -0400

    work

---
+++ src/components/Game/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class game extends Component{
+    render(){
+        return{
+
+        }
+    }
+}
+
+export default 'game';
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './nav.scss';
+
+function nav (props) {
+    return(
+    <nav className="Navbar">
+    <h1>clicky game</h1>
+    </nav>
+    );
+}
+
+export default nav;
